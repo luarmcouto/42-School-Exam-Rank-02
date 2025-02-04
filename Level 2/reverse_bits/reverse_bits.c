4f02473665e3fa327ce057b20189b12ac33db522
luarodri <luarmcouto@icloud.com>
2025-02-04 12:05:12 +0000

    att

--- Level 2/reverse_bits/reverse_bits.c
+++ Level 2/reverse_bits/reverse_bits.c
@@ -1,13 +1,14 @@
-unsigned char	reverse_bits(unsigned char octet)
+unsigned char reverse_bits(unsigned char octet)
 {
-	int		i = 8;
-	unsigned char	res = 0;
+    int i = 8;
+    unsigned char res = 0;
 
-	while (i > 0)
-	{
-		res = res * 2 + (octet % 2);
-		octet = octet / 2;
-		i--;
-	}
-	return (res);
+    while (i > 0)
+    {
+        res = (res << 1) | (octet & 1);  // Desloca res para a esquerda e adiciona o bit menos significativo de octet
+        octet = octet >> 1;  // Desloca octet para a direita para pegar o próximo bit
+        i--;
+    }
+    return (res);
 }
+
